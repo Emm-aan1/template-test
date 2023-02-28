a5ad969970fe0c57abeca1089b8bca6ec8dbae51
Chidoka Ifeanyi <114946207+Emm-aan1@users.noreply.github.com>
2023-02-28 20:35:55 +0100

    new updates

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('testing');
+// console.log('testing');
+
+// fill array with 60000 elements 
+const list = new Array(60000).join('1.1').split('.');
+
+function removeItemsFromList() {
+  var item = list.pop();
+
+  if (item) {
+    setTimeout(removeItemsFromList, 1000);
+  }
+};
+removeItemsFromList();
